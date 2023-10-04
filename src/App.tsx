faefde514c35508cb4411aeb6813e979f3cfc751
pv-kiran <kiranpv903@gmail.com>
2023-10-04 11:12:00 +0530

    added navbar config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import Navbar from "./components/NAvbar";
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Navbar></Navbar>
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/store" element={<Store />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </Container>
     </>
   );
 }
